9d77d08334ce5104fe9075076ca04b33c4bf996f
will moore <will.moore@aarons.com>
2018-06-30 14:02:35 -0400

    initial commit

---
+++ CookbookVersionReader/CookbookVersionReader.ps1
@@ -0,0 +1,69 @@
+﻿Param (
+    [string]$searchPattern = "**\metadata.rb",
+	[string]$variablesPrefix
+)
+
+# Write all params to the console.
+Write-Host ("Search Pattern: " + $searchPattern)
+Write-Host ("Variables Prefix: " + $variablesPrefix)
+
+function SetBuildVariable([string]$varName, [string]$varValue)
+{
+	Write-Host ("Setting variable " + $variablesPrefix + $varName + " to '" + $varValue + "'")
+	Write-Output ("##vso[task.setvariable variable=" + $variablesPrefix + $varName + ";]" +  $varValue )
+}
+
+function SetAssemblyVariables($content)
+{
+    $matches = [regex]::Matches($content, "[']([0-9]+).([0-9]+).([0-9]+)[']")
+
+    if($matches.Success)
+    {
+        foreach($match in $matches)
+        {
+            if($match.Groups.Count -eq 4 -and $match.Groups[0] -ne '')
+            {
+				$prefix = "Version"
+
+                        if ($match.Groups[1].Success)
+                        {
+                            $major = $match.Groups[1].Value
+                            SetBuildVariable "$prefix.Major" $major
+
+                            if ($match.Groups[2].Success)
+                            {
+                                $minor = $match.Groups[2].Value
+                                SetBuildVariable "$prefix.Minor" $minor
+
+                                if ($match.Groups[3].Success)
+                                {
+                                    $build = $match.Groups[3].Value
+                                    SetBuildVariable "$prefix.Patch" $build
+                                    SetBuildVariable "$prefix.Build" $build
+                                }
+                             }
+                        }
+             }
+         }
+    }
+ }
+
+
+$filesFound = Get-ChildItem -Path $searchPattern -Recurse
+
+if ($filesFound.Count -eq 0)
+{
+    Write-Warning ("No files matching pattern found.")
+}
+
+if ($filesFound.Count -gt 1)
+{
+   Write-Warning ("Multiple assemblyinfo files found.")
+}
+
+foreach ($fileFound in $filesFound)
+{
+    Write-Host ("Reading file: " + $fileFound)
+    $fileText = [IO.File]::ReadAllText($fileFound)
+    SetAssemblyVariables($fileText)
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+﻿# Description
+
+VSTS build task to read chef cookbook version number from metadata.rb file
+
+Reads the version number from metadata.rb into environment variables for use in incrementing cookbook versions.
+
+Optionally prefix the variable values so multiple projects can be read.
+
+Adapted from [AssemblyInfoReaderTask](https://github.com/kyleherzog/AssemblyInfoReaderTask) with thanks to [kyleherzog](https://github.com/kyleherzog)
